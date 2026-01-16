b320ebf3b42712f87b52472ef6146f85c60e8347
Mangino <26ManginoN@badinhs.org>
2026-01-16 12:32:52 -0500

    finished activity

--- DOMpractice/DOMactivty.js
+++ DOMpractice/DOMactivty.js
@@ -4,13 +4,32 @@ content.classList.add("content");
 content.textContent = "This is the glorious text-content!"; 
 container.appendChild(content);
 
+
 const p = document.createElement("p");
-p.textContent = "Im red";
-p.style.color = " #ff0011";    
+p.textContent = "Hey Im red";
+p.style.color = "red";    
+container.appendChild(p); 
 
 const h3 = document.createElement("h3");
-h3.textContent = " I’m a blue h3!";
+h3.textContent = " I am a blue h3!";
 h3.style.color = "blue";
+container.appendChild(h3);
+
+const div = document.createElement("div");
+div.style.border = "3px black";
+div.style.backgroundColor = "pink";
+container.appendChild(div);
+
+const insideH1 = document.createElement("h1");
+insideH1.textContent = "Im in a div";
+div.appendChild(insideH1);
+
+const insideP = document.createElement("hi");
+insideP.textContent = "Me too";
+div.appendChild(insideP);
+
+
+
 
 
 
